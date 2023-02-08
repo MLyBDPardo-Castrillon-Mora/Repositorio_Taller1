f8842e06fa38ef04ebb535ed86fa4ef0981c5f3d
RiemannCauchy <123638911+RiemannCauchy@users.noreply.github.com>
2023-02-08 16:52:12 -0500

    Codigo listo! - faltan figuras, pero nada grande

--- scripts/Taller 1 - main.R
+++ scripts/Taller 1 - main.R
@@ -55,10 +55,12 @@ db <- db[db$ocu==1,]
 # Eliminar observaciones sin salario
 temp <- temp[temp$y_ingLab_m>0,]
 db <- db[db$y_ingLab_m>0,]
+db <- db[!is.na(db$age),]
 
 # Generar log de los ingresos
 temp$log_w = log(temp$y_ingLab_m) 
 db$log_w = log(db$y_ingLab_m)
+
 
 
 # AGE-WAGE PROFILE =============================================================
@@ -135,8 +137,12 @@ model_3 <- lm(log_w~age+sex+interac_age_sex, data = part_db$p_0.7)
 model_4 <- lm(log_w~age+I(age^2)+sex+interac_age_sex, data = part_db$p_0.7)
 model_5 <- lm(log_w~age+I(age^2)+I(age^3)+sex+interac_age_sex, data = part_db$p_0.7)
 
+model_6 <- lm(log_w~age+I(age^2), data = part_db$p_0.7)
+model_7 <- lm(log_w~sex, data = part_db$p_0.7)
+model_8 <- lm(log_w~age+maxEducLevel+formal+hoursWorkUsual+oficio, data = part_db$p_0.7)
+
 # lista de modelos 
-models <- list(model_1, model_2, model_3, model_4, model_5)
+models <- list(model_1, model_2, model_3, model_4, model_5, model_6, model_7, model_8)
 aictab(cand.set = models)
 bictab(cand.set = models)
 
@@ -147,6 +153,10 @@ forecast_3 <- predict(model_3, newdata = part_db$test)
 forecast_4 <- predict(model_4, newdata = part_db$test)
 forecast_5 <- predict(model_5, newdata = part_db$test)
 
+forecast_6 <- predict(model_6, newdata = part_db$test)
+forecast_7 <- predict(model_7, newdata = part_db$test)
+forecast_8 <- predict(model_8, newdata = part_db$test)
+
 # MSE
 p_aux <- data.frame(
   pred_1 = forecast_1, 
@@ -154,6 +164,9 @@ p_aux <- data.frame(
   pred_3 = forecast_3,
   pred_4 = forecast_4,
   pred_5 = forecast_5,
+  pred_6 = forecast_6,
+  pred_7 = forecast_7,
+  pred_8 = forecast_8,
   actual = part_db$test$log_w
 )
 
@@ -163,25 +176,29 @@ mean((p_aux$actual - p_aux$pred_3)^2)
 mean((p_aux$actual - p_aux$pred_4)^2)
 mean((p_aux$actual - p_aux$pred_5)^2)
 
+mean((p_aux$actual - p_aux$pred_6)^2)
+mean((p_aux$actual - p_aux$pred_7)^2)
+mean((p_aux$actual - p_aux$pred_8)^2)
+
 # LOOCV - Modelos 4 y 5
 
-CV4_aux <- rep(0,1000)
-for (i in 1:16277) {
-  CV4 <- lm(log_w~age+I(age^2)+sex+interac_age_sex, data = db[-i,])
-  F_4 <- predict(CV4, newdata = db[i,])
-  CV4_aux[i] <- (db$log_w[i] - F_4)
+CV8_aux <- rep(0,9892)
+for (i in 1:9892) {
+  CV8 <- lm(log_w~age+maxEducLevel+formal+hoursWorkUsual+oficio, data = db[-i,])
+  F_8 <- predict(CV8, newdata = db[i,])
+  CV8_aux[i] <- (db$log_w[i] - F_8)
 }
-CV4_aux <- CV4_aux^2
-CV_4 <- 1/16277*sum(CV4_aux)
+CV8_aux <- CV8_aux^2
+CV_8 <- 1/9891*sum(CV8_aux, na.rm=TRUE)
 
 
-CV5_aux <- rep(0,1000)
-for (i in 1:16277) {
+CV5_aux <- rep(0,9892)
+for (i in 1:9892) {
   CV5 <- lm(log_w~age+I(age^2)+I(age^3)+sex+interac_age_sex, data = db[-i,])
   F_5 <- predict(CV5, newdata = db[i,])
   CV5_aux[i] <- (db$log_w[i] - F_5)
 }
 CV5_aux <- CV5_aux^2
-CV_5 <- 1/16277*sum(CV5_aux)
+CV_5 <- 1/9892*sum(CV5_aux)
 
 
